cbab5195b25afc39cfdef27509b0dfc0d11926f8
gene-yoo <gene.y.yoo@gmail.com>
2017-10-17 11:37:09 -0400

    Lab complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,33 @@
-class Post < ActiveRecord::Base
+class SpamValidator < ActiveModel::Validator
+  def validate(post)
+    clickbait = ["Won't Believe", "Secret", "Top #{/[0-9]/}", "Guess"]
+    if (post.title != nil) && clickbait.none? { |bait| post.title.match(bait) }
+      post.errors[:title] << "must include clickbait"
+    end
+  end
 end
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  include ActiveModel::Validations
+  validates_with SpamValidator
+end
+
+# class Post < ActiveRecord::Base
+#   validates :title, presence: true
+#   validates :content, length: { minimum: 250 }
+#   validates :summary, length: { maximum: 250 }
+#   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+#   validate :check_clickbait
+#
+#   def check_clickbait
+#     clickbait = ["Won't Believe", "Secret", "Top #{/[0-9]/}", "Guess"]
+#     if (self.title != nil) && clickbait.none? { |bait| self.title.match(bait) }
+#       errors.add(:title, "must include clickbait")
+#     end
+#   end
+# end
